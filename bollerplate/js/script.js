8af8658c717d7299289af8853fa7f0955e1f457e
adshes <42263959+adshes@users.noreply.github.com>
2018-08-28 16:04:54 +0300

    new

--- bollerplate/js/script.js
+++ bollerplate/js/script.js
@@ -10,7 +10,28 @@ var j = prompt('спросит сколько вы купили яблок');
 var u = prompt('сколько стоит одно яблоко');
 
 var k = prompt('сколько стоит один батон хлеба');
-var c= i*k+j*u;
+//var c= i*k+j*u;
 
-var s = x>c;
-document.write(s);
+//var s = x>c;
+//document.write(s);
+
+ function haveEnough(x, i, j, u, k) {
+
+
+	//haveEnough(document.body, 'Вам хватает денег на покупки');
+
+
+	//haveEnough(document.body, 'Вам не хватает денег на покупки');
+	//document.write(haveEnough);
+if (x>=(i*k+j*u)) {
+
+return('Вам хватает денег на покупки');
+
+} else{
+	return('Вам не хватает денег');
+
+}
+
+
+}
+

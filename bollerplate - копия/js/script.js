330760c6db44da483f1ee2f4bd45f7a016f6335f
adshes <42263959+adshes@users.noreply.github.com>
2018-09-06 16:37:10 +0300

    hi

--- bollerplate - копия/js/script.js
+++ bollerplate - копия/js/script.js
@@ -1,13 +1,64 @@
-/*34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
-46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44];*/
-var i = 0;
-var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
-var hasBubbleGum = [false, false, false, true];
-while (i<hasBubbleGum.length){
-	if (hasBubbleGum[i]){
-		console.log(products[i] +" contains");
-		i = i+1
-	}
+var scores = [60, 50, 60, 58, 54, 54,
+58, 50, 52, 54, 48, 69,
+34, 55, 51, 52, 44, 51,
+69, 64, 66, 55, 52, 61,
+46, 31, 57, 52, 44, 18,
+41, 53, 55, 61, 51, 44];
+var costs = [.25, .27, .25, .25, .25, 
+.25,.33, .31, .25, .29, .27, 
+.22,.31, .25, .25, .33, .21, 
+.25,.25, .25, .28, .25, .24, 
+.22,.20, .25, .30, .25, .24, .25,
+.25, .25, .27, .25, .26, .29];
 
+
+//var products = ["Choo Choo Chocolate", "Icy Mint", "Cake Batter", "Bubblegum"];
+//var hasBubbleGum = [false, false, false, true];
+function printandget(scores){
+var hight = 0;
+
+for (var i = 0; i<scores.length; i++)
+{
+console.log("Bubble solution #" + i +" score: " + scores[i]);
 	
+if (hight<scores[i] ){
+	hight = scores[i];
+	
+}}
+return hight;
 }
+var hight = printandget(scores);
+console.log("Bubbles tests: " + scores.length);
+console.log("Highest bubble score: " + hight);
+//console.log(hight);
+
+function mylist1(scores, hight){
+	var mylist = [];
+     for (var i = 0; i<scores.length; i++){
+
+	if (hight==scores[i]){
+		mylist.push(i);
+		//.log(mylist);
+	}
+}
+return mylist;
+}
+var mylist = mylist1(scores, hight);
+console.log("Solutions with the highest score: " + mylist);
+
+function getmost (scores, costs, hight){
+
+	var cost = 100;
+	var index;
+	for (i=0; i<scores.length; i++){
+		if (hight==scores[i]){
+			if(cost > costs[i]){
+				index =i;
+				cost= costs[i];
+			}
+		}
+}
+return index;
+}
+var mostCostEffective = getmost (scores, costs, hight);
+console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
